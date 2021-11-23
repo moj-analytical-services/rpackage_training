3bf3ff4f09c9365c4e71ef5c7ff36427f92b2e02
aidanmews <32537539+aidanmews@users.noreply.github.com>
2021-11-23 10:54:12 +0000

    Update summarise_crimes.R
    Change function name from filer_crimes to summarise_crimes

--- summarise_crimes.R
+++ summarise_crimes.R
@@ -2,7 +2,7 @@
 
 library(dplyr)
 
-filter_crimes <- function(data_set, years) {
+summarise_crimes <- function(data_set, years) {
   
   filtered <- filter(data_set, year %in% years)
   summarise(filtered, crimes = mean(crimes))
